4d85a9918f5dac3d29333efc8c2aab08e6a47ad4
Nikee <rivera.nikee@gmail.com>
2018-07-02 14:35:23 -0400

    added more tabe to html and small changes to app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+//========================= Firebase info =========================//
+
+//========================= App Functions =========================//
+$(document).ready(function() {
+// Empty array that contains the train data.
+var trainInfo = [];
+
+// Function that initializes the table data.
+});
